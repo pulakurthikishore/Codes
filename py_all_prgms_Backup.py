a9ee7acc1fb1a95f31ca8a3a434c711dbf1a7898
kishore <kishore@gmail.com>
2020-04-03 09:01:16 +0530

    changes  of py file

--- py_all_prgms_Backup.py
+++ py_all_prgms_Backup.py
@@ -9,6 +9,20 @@
 
 /* functions Related examples   */ 
 
+recursive program :
+
+def f1(n):
+	if n ==0:
+		result =1
+	else:
+	    result = n*f1(n-1)
+	return result
+s1=f1()
+print(s1)	    	
+
+
+
+
 
 
 
