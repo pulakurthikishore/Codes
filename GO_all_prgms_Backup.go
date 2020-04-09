1e65b08b7e590417b4a1a565356324dd0bb5dbaa
kishore <kishore@gmail.com>
2020-04-09 23:56:00 +0530

    lap config on py file

--- GO_all_prgms_Backup.go
+++ GO_all_prgms_Backup.go
@@ -2,5 +2,31 @@
 ------------------------------ Golang all Practice Examples---------------------------------------                      
 
 
+//Loops and switch examples 
+
+print every number from 1 to 10000
+
+package main
+import "fmt"
+func main() {
+
+for x:=1;x<=10000;x++ {
+	fmt.Println(x)
+}
+}
+
+
+package main
+import "fmt"
+func main() {
+
+x:=1
+for x<=10000{
+	fmt.Println(x)
+	x++
+}
+}
+
+
 
 */
--- py_all_prgms_Backup.py
+++ py_all_prgms_Backup.py
@@ -21,4 +21,26 @@ s1=f1()
 print(s1)	    	
 
 
+Lambda:
+------
+s=lambda n:n*n
+print(s(2))
+
+
+
+System configaration for VR game Developments 
+
+@@@@@
+
+Gaming PC 
+
+
+Processor   : i7   7th or 8th gen       // which defines the speed     min i5 processor  >6th gen 
+motherboard : 
+Ram         : 12              // min 8              DDR4   8gb min 
+
+Graphic     : NVIDA                           // GEforece >700 series 
+
+
+@@@@@
 '''
